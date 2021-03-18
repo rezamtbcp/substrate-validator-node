9d7e926bc0af7c10ac2373ccce2309c7e3c84e01
rezamtbcp <80309741+rezamtbcp@users.noreply.github.com>
2021-03-18 16:34:52 +1100

    Adding role arn

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,10 @@
 provider "aws" {
   region = var.aws_region
+  
+  assume_role {
+    role_arn     = var.iam_role_arn
+    session_name = "TerraformBuilder"
+  }
 }
 
 data "aws_availability_zones" "available" {
